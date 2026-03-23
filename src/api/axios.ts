81c8974c5b9712161c74cf6f2c5d7fe8a89d1fd1
n4mle <namle26022004@gmail.com>
2026-03-23 14:34:45 +0700

    update backend url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL || 'https://commerce-backend-mpun.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
